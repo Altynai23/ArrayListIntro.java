30ca61088ccbd36315dd674ab8f0f4362ea7dadf
Altynai23 <salyb1987@gmail.com>
2021-08-02 12:34:35 -0400

    day38

--- src/day38_CustomClass/RectangleObjects.java
+++ src/day38_CustomClass/RectangleObjects.java
@@ -1,5 +1,7 @@
 package day38_CustomClass;
 
+import java.util.Arrays;
+
 public class RectangleObjects {
 
     public static void main(String[] args) {
@@ -10,18 +12,28 @@ public class RectangleObjects {
         rectangle1.length = 20;
         */
         rectangle1.setInfo( 30, 20);
+        // rectangle1.getInfo();
 
-        rectangle1.getInfo();
+        System.out.println(rectangle1);
+
 
         System.out.println("---------------------------------------------");
         // l:50, w:10
         Rectangle rectangle2 = new Rectangle();
         rectangle2.setInfo(50, 10);
 
-        rectangle2.getInfo();
+        //rectangle2.getInfo();
 
+        System.out.println(rectangle2);
 
+        System.out.println("---------------------------------------------");
 
+        Rectangle[] arr = {rectangle1, rectangle2};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+
+        System.out.println(Arrays.toString(arr));
 
     }
 }
