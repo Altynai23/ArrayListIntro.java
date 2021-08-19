068b35e563f68850022f34efd1423fe9b653ab73
Altynai23 <salyb1987@gmail.com>
2021-08-19 11:57:14 -0400

    day47

---
+++ src/day47_Recap/browsersTask/BrowserObjects.java
@@ -0,0 +1,19 @@
+package day47_Recap.browsersTask;
+
+public class BrowserObjects {
+
+    public static void main(String[] args) {
+        Chrome chrome = new Chrome(); //opens firefox browser
+        chrome.open();
+
+        FireFox firefox = new FireFox(); //opens firefox browser
+        firefox.open();
+
+
+
+
+
+    }
+
+
+}
